ac40f17e99cc99d9805efbe5dd5f59861cb8a20a
Zoe Guidry <oyezoey@gmail.com>
2023-07-28 15:01:29 -0500

    lab completed

--- server/app.py
+++ server/app.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask # import flask
 
-app = Flask(__name__)
+app = Flask(__name__) # flask constructor
+
+@app.route("/")
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route("/<string:username>")
+def name(username):
+    return  f'<h1>Profile for {username}</h1>'
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
